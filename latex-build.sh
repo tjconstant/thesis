21e61a5c7b125dfa529fccac443b44f8aa6e47a9
Tom Constant <t.j.constant@gmail.com>
2018-04-17 11:39:19 +0100

    changed build engine to xelatex

--- latex-build.sh
+++ latex-build.sh
@@ -1,5 +1,5 @@
-pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
+xelatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
 bibtex thesis
-pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
-pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
+xelatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
+xelatex-synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
 ./latex-clean.sh
