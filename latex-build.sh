e21741438effcbee074942588a7432fbfe0e786e
Tom Constant <t.j.constant@gmail.com>
2018-04-17 13:33:38 +0100

    fixed references

--- latex-build.sh
+++ latex-build.sh
@@ -1,5 +1,7 @@
 xelatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
 bibtex thesis
+makeindex thesis
 xelatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
-xelatex-synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
+makeindex thesis
+xelatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
 ./latex-clean.sh
