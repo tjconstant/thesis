55f405aa2f57a90d7bc589ebbbfebf0ada6bfe22
Tom Constant <t.j.constant@gmail.com>
2018-04-17 10:41:10 +0100

    Update latex-build.sh

--- latex-build.sh
+++ latex-build.sh
@@ -2,4 +2,3 @@ pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesi
 bibtex thesis
 pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
 pdflatex -synctex=1 -interaction=nonstopmode -shell-escape -enable-write18 thesis.tex
-latex-clean.sh
